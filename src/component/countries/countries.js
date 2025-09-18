5a7a084e3f1aa06d3dd922bac156df3984a853ad
SanjidaMostakPrena <sanjidaprena7@gmail.com>
2025-09-18 22:39:39 +0600

    first commit

---
+++ src/component/countries/countries.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const countries = () => {
+    return (
+        <div>
+            <h1>In the countries</h1>
+        </div>
+    );
+};
+
+export default countries;
+
