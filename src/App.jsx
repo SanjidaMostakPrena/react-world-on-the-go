3618ad298412e268c7a74821cf36d556de8dbc47
SanjidaMostakPrena <sanjidaprena7@gmail.com>
2025-09-22 18:57:11 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 
 import './App.css'
+import Countries from './component/countries/countries'
 
 function App() {
-  
-
   return (
     <>
-     
-      <countries></countries>
-     
+      <Countries></Countries>
     </>
   )
 }
---
+++ src/component/countries/countries.jsx
@@ -0,0 +1,8 @@
+function Countries() {
+    return (
+        <div>
+            <h1>IN THE COUNTRIES </h1>
+        </div>
+    )
+}
+export default Countries
